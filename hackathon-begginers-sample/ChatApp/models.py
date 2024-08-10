dedffd776df364cb78e005a641d8b7c431c78924
ishiyamatomoaki22 <t-ishiyama@aile-283.com>
2024-08-11 02:19:04 +0900

    [新規作成する映画ルームの情報をデータベースに登録する処理を追加]

--- hackathon-begginers-sample/ChatApp/models.py
+++ hackathon-begginers-sample/ChatApp/models.py
@@ -9,7 +9,7 @@ class dbConnect:
             cur.execute(sql, (id, name, email, password))
             conn.commit()
         except Exception as e:
-            print(e + 'が発生しています')
+            print(e)
         finally:
             cur.close()
 
@@ -23,6 +23,47 @@ class dbConnect:
             user = cur.fetchone()
             return user
         except Exception as e:
-            print(e + 'が発生しています')
+            print(e)
+        finally:
+            cur.close()
+
+    def getMovieRecords():
+        try:
+            # データベースに接続
+            conn = DB.getConnection()
+            cur = conn.cursor()
+            # データを取得
+            sql = "SELECT * FROM movies;"
+            cur.execute(sql)
+            movie_records = cur.fetchall()
+            return movie_records
+        except Exception as e:
+            print(e)
+        finally:
+            # 接続を閉じる
+            cur.close()
+
+    def getMovieRoomRecord(movie_id):
+        try:
+            conn = DB.getConnection()
+            cur = conn.cursor()
+            sql = "SELECT m.id,m.movie_title,mr.movie_id FROM movies AS m LEFT JOIN movierooms AS mr ON m.id = mr.movie_id WHERE m.id='%s';"
+            cur.execute(sql,int((movie_id)))
+            movieroom_record = cur.fetchone()
+            return movieroom_record
+        except Exception as e:
+            print(e)
+        finally:
+            cur.close()
+
+    def addMovieRoom(user_id,movie_id):
+        try:
+            conn = DB.getConnection()
+            cur = conn.cursor()
+            sql = "INSERT INTO movierooms (user_id, movie_id) VALUES (%s, %s);"
+            cur.execute(sql, (user_id, int(movie_id)))
+            conn.commit()
+        except Exception as e:
+            print(e)
         finally:
             cur.close()
